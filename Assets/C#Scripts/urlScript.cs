27878e8d0d08d9462d95ba7d2b08aabb4ed99141
photosislandi-max <photosislandi@gmail.com>
2025-09-22 16:12:58 +0200

    forhaabentligt virker dette fine stykke arbejde
    ...

---
+++ Assets/C#Scripts/urlScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void Openurl()
+    {
+        Application.OpenURL("https://www.aau.dk/uddannelser/bachelor/liste/koebenhavn");
+    }
+}
